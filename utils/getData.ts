a2017edf6358304c7dbd6a9aaadbc1b2122d6208
Fabio T Alves <ftalves@poli.ufrj.br>
2021-07-25 22:32:29 -0300

    Bugfix: localStorage on prerender

--- utils/getData.ts
+++ utils/getData.ts
@@ -1,34 +1,42 @@
 import { clientType, productType, orderType } from "utils/types";
-import {presetProducts} from "database/products"
+import { presetProducts } from "database/products";
 
 export const getClients = (): clientType[] => {
-  const clients = localStorage.getItem(`@clientList`);
-  if (clients) {
-    return JSON.parse(clients);
+  if (typeof window !== "undefined") {
+    const clients = localStorage.getItem(`@clientList`);
+    if (clients) {
+      return JSON.parse(clients);
+    }
   }
   return [];
 };
 
 export const getCart = (): productType[] => {
-  const cartStorage = localStorage.getItem(`@cart`);
-  if (cartStorage) {
-    return JSON.parse(cartStorage);
+  if (typeof window !== "undefined") {
+    const cartStorage = localStorage.getItem(`@cart`);
+    if (cartStorage) {
+      return JSON.parse(cartStorage);
+    }
   }
   return [];
 };
 
 export const getProducts = (): productType[] => {
-  const products = localStorage.getItem(`@productList`);
-  if (products) {
-    return JSON.parse(products);
+  if (typeof window !== "undefined") {
+    const products = localStorage.getItem(`@productList`);
+    if (products) {
+      return JSON.parse(products);
+    }
   }
   return presetProducts;
-}
+};
 
 export const getOrders = (): orderType[] => {
-  const orders = localStorage.getItem(`@orderList`);
-  if (orders) {
-    return JSON.parse(orders);
+  if (typeof window !== "undefined") {
+    const orders = localStorage.getItem(`@orderList`);
+    if (orders) {
+      return JSON.parse(orders);
+    }
   }
   return [];
-}
+};
